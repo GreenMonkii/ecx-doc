9d20c4b21ee9455461cc0cc4f30e87796100bf9d
Truelife Agada <abramsharry6@gmail.com>
2025-06-14 23:24:17 +0100

    style: Clean up whitespace in groupAnagrams function

--- group-anagrams.py
+++ group-anagrams.py
@@ -1,16 +1,17 @@
 from collections import defaultdict
 
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         # Use a defaultdict to group anagrams so new keys automatically get an empty list.
         anagram_map = defaultdict(list)
-        
+
         for word in strs:
-            # Anagrams will have identical sorted signatures (e.g., "eat", "tea", "ate" all become "aet").
-            sorted_word = ''.join(sorted(word))
-            
+            # Anagrams will have identical sorted signatures.
+            sorted_word = "".join(sorted(word))
+
             # Append the original word to the list associated with its sorted signature.
             anagram_map[sorted_word].append(word)
-        
+
         # The values of the map are the lists of grouped anagrams.
         return list(anagram_map.values())
